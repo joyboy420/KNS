da0788ca17e0406b9f4a0c84bc320d9cf86c26ad
joyboy420 <progress198@gmail.com>
2020-09-06 09:55:36 +0545

    incomplete frontend design

---
+++ app.js
@@ -0,0 +1,17 @@
+(function ($) {
+
+    'use strict';
+
+    // STICKY
+    $(window).scroll(function () {
+        var scroll = $(window).scrollTop();
+
+        if (scroll >= 50) {
+            $(".sticky").addClass("nav-sticky");
+        } else {
+            $(".sticky").removeClass("nav-sticky");
+        }
+    });
+
+
+})(jQuery)
